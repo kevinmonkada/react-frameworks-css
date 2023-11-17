49c95403229ece98c8ef05e6b96a438f68872f5e
Kevin Moncada <kevinmonkada@gmail.com>
2023-11-16 19:11:21 -0500

    Testing how React works with Frameworks

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import MaterialUI from "./components/MaterialUI";
+/* import Bulma from "./components/Bulma"; */
+/* import ReactBootstrap from "./components/ReactBootstrap"; */
+/* import Bootstrap from "./components/Bootstrap"; */
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Frameworks CSS con React</h1>
+        {/* <Bootstrap /> */}
+        {/* <Bulma /> */}
+        <MaterialUI />
+      </div>
+    </>
+  );
+}
+
+export default App;
